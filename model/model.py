6c79d4897dbe6146b97137640f3b9defef03ffe6
Ali Yaghoobian <ali.yaghoobiann@gmail.com>
2023-03-26 06:49:58 +0430

    Train image classification model

---
+++ model/model.py
@@ -0,0 +1,35 @@
+import json
+import numpy as np
+from pathlib import Path
+from tensorflow.image import resize
+from tensorflow.keras.models import load_model
+
+
+
+class AIModel:
+    model_path: Path
+    model = None
+
+    def __post_init__(self):
+        if self.model_path.exists():
+            self.model = load_model(self.model_path)
+
+    def get_model(self):
+        if not self.model:
+            raise Exception("Model not implemeted")
+        return self.model
+    def preprocessing(self, image_query):
+        # Resize to 32*32
+        x_pred = resize(image_query,[32,32]).astype('float32')
+        # Scale data
+        x_input = x_pred / 255
+        return x_input
+
+    def predict(self, image_query):
+        model = self.get_model()
+        x_input = self.preprocessing(image_query)
+        preds = model.predict(x_input)[0]
+        # Convert One-hot vector to index
+        idx = np.argmax(preds,axis=1)
+        labels= ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+        return labels[idx]
